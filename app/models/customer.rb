98bab6e3a198d0c0ed64a647900e96d8bf8bf7bd
Andrew Curley <andrew.curley@gmail.com>
2011-09-28 19:23:26 -0400

    Initial commit of local Customer model

---
+++ app/models/customer.rb
@@ -0,0 +1,9 @@
+require "#{Hydraulics.models_dir}/customer"
+
+class Customer
+
+  # validates :uva_status, 
+  #           :presence => true, 
+  #           :message => "The 'UVa Status' associated with this Customer no longer exists."
+
+end
