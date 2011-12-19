f33a732276c77d4174f2c734feaec901cf0c7196
Andrew Curley <andrew.curley@gmail.com>
2011-12-19 12:48:38 -0500

    Use active record store for session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Tracksys::Application.config.session_store :cookie_store, :key => '_tracksys_session'
+# Tracksys::Application.config.session_store :cookie_store, :key => '_tracksys_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Tracksys::Application.config.session_store :active_record_store
+  Tracksys::Application.config.session_store :active_record_store
