d4722fef2f4e0d202d0177619d696a153740bf3e
Andrew Curley <andrew.curley@gmail.com>
2012-01-26 11:42:30 -0500

    Bibl.title is now aliased as Bibl.name for ActiveAdmin

--- app/models/bibl.rb
+++ app/models/bibl.rb
@@ -48,4 +48,11 @@
 require "#{Hydraulics.models_dir}/bibl"
 
 class Bibl
+
+  #------------------------------------------------------------------
+  # aliases
+  #------------------------------------------------------------------
+  # Necessary for Active Admin to poplulate pulldown menu
+  alias_attribute :name, :title
+
 end
