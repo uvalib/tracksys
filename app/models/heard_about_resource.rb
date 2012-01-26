89027ad5d69ffc3060ae84e8460f5c34ea1955bf
Andrew Curley <andrew.curley@gmail.com>
2012-01-26 11:42:59 -0500

    HeardAboutResource.description is not aliased as .name for ActiveAdmin

--- app/models/heard_about_resource.rb
+++ app/models/heard_about_resource.rb
@@ -13,4 +13,11 @@
 require "#{Hydraulics.models_dir}/heard_about_resource"
 
 class HeardAboutResource
+
+  #------------------------------------------------------------------
+  # aliases
+  #------------------------------------------------------------------
+  # Necessary for Active Admin to poplulate pulldown menu
+  alias_attribute :name, :description  
+  
 end
