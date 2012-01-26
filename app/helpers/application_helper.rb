8e38c2c6eac2b92985a41f9e869a55df663f9370
Andrew Curley <andrew.curley@gmail.com>
2012-01-26 11:50:08 -0500

    Added two helper methods to deal with display of email addresses and dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 require "#{Hydraulics.helpers_dir}/application_helper"
 
 module ApplicationHelper
+
+  def format_date(date)
+    begin
+      return date.strftime("%B %d, %Y")
+    rescue Exception => e
+      return nil
+    end
+  end
+
+  def format_email_in_sidebar(email)
+    begin
+      return email.gsub(/(@)/, "@\r").delete(' ')
+    rescue Exception => e
+      return nil
+    end
+  end
+
 end
