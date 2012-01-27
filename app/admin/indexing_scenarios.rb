e27235127ac861defef17200b5cd3527d0fa5394
Andrew Curley <andrew.curley@gmail.com>
2012-01-27 14:53:28 -0500

    Added custom index view for Admin::IndexingScenarios

--- app/admin/indexing_scenarios.rb
+++ app/admin/indexing_scenarios.rb
@@ -1,4 +1,12 @@
 ActiveAdmin.register IndexingScenario do
   menu :parent => "Miscellaneous"
+
+  index do
+    column :name
+    column :pid
+    column :datastream_name
+    column :repository_url
+    default_actions
+  end
   
 end
