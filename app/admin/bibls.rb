71fbb446c0aa447047fdeaac759e33504f30d0f7
Andrew Curley <andrew.curley@gmail.com>
2012-02-07 11:53:11 -0500

    Updated show partial for Admin::Bibl

--- app/admin/bibls.rb
+++ app/admin/bibls.rb
@@ -6,6 +6,7 @@ ActiveAdmin.register Bibl do
   scope :not_approved
 
   filter :title
+  filter :call_number
   filter :creator_name
   filter :catalog_id, :label => "Catalog Key"
   filter :barcode
@@ -25,6 +26,26 @@ ActiveAdmin.register Bibl do
   end
   
   show do
+    panel "Units" do
+      table_for bibl.units do
+        column("ID") {|u| link_to "#{u.id}", admin_unit_path(u)}
+        column ("DL Status") {|unit|
+          case 
+            when unit.include_in_dl?
+              Unit.human_attribute_name(:include_in_dl)
+            when unit.exclude_from_dl?
+              Unit.human_attribute_name(:exclude_from_dl)
+          end
+        }
+        column :date_archived
+        column :date_patron_deliverables_ready
+        column :date_queued_for_ingest
+        column :date_dl_deliverables_ready
+        column :intended_use
+        column :master_files_count
+      end
+    end
+
     panel "Automation Messages" do
       table_for bibl.automation_messages do
         column("ID") {|am| link_to "#{am.id}", admin_automation_message_path(am)}
