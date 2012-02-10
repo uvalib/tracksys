6f3529967195e64dc68c48ffe9a1ef786aa67dbb
Andrew Curley <andrew.curley@gmail.com>
2012-02-10 16:00:51 -0500

    Add foreign keys between LegacyIdentifiersMasterFiles and both MasterFile and LegacyIdentifier

--- db/migrate/017_upgrade_legacy_identifiers_master_files.rb
+++ db/migrate/017_upgrade_legacy_identifiers_master_files.rb
@@ -2,5 +2,8 @@ class UpgradeLegacyIdentifiersMasterFiles < ActiveRecord::Migration
   def change
     rename_index :legacy_identifiers_master_files, 'legacy_identifier_id', 'index_legacy_identifiers_master_files_on_legacy_identifier_id'
     rename_index :legacy_identifiers_master_files, 'master_file_id', 'index_legacy_identifiers_master_files_on_master_file_id'
+    
+    add_foreign_key :legacy_identifiers_master_files, :master_files
+    add_foreign_key :legacy_identifiers_master_files, :legacy_identifiers
   end
 end
