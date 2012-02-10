479bd9be8b5b3f9a838889d94276ee2824563af3
Andrew Curley <andrew.curley@gmail.com>
2012-02-10 16:00:04 -0500

    Add foreign key between ImageTechMeta and MasterFiles

--- db/migrate/018_upgrade_image_tech_meta.rb
+++ db/migrate/018_upgrade_image_tech_meta.rb
@@ -1,6 +1,8 @@
-class UpgradeComponents < ActiveRecord::Migration
+class UpgradeImageTechMeta < ActiveRecord::Migration
   def change
     # All images use dpi as the resolution_unit, so there is no reason to keep this column
     remove_column :image_tech_meta, :resolution_unit
+    
+    add_foreign_key :image_tech_meta, :master_files
   end
 end
