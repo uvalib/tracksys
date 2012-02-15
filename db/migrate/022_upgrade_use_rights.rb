b094711c4ee66a6cf2b49e945704c285bb65e30a
Andrew Curley <andrew.curley@gmail.com>
2012-02-15 17:31:29 -0500

    Update UseRight.update_counters in migration

--- db/migrate/022_upgrade_use_rights.rb
+++ db/migrate/022_upgrade_use_rights.rb
@@ -4,5 +4,14 @@ class UpgradeUseRights < ActiveRecord::Migration
     add_column :use_rights, :bibls_count, :integer, :default => 0
     add_column :use_rights, :components_count, :integer, :default => 0
     add_column :use_rights, :master_files_count, :integer, :default => 0
+    add_column :use_rights, :units_count, :integer, :default => 0
+
+    say "Updating use_right.orders_count, use_right.units_count, use_right.components_counts and use_right.master_files_count"
+    UseRight.find(:all).each {|u|
+      UseRight.update_counters u.id, :orders_count => u.orders.count
+      UseRight.update_counters u.id, :units_count => u.units.count
+      UseRight.update_counters u.id, :components_count => u.components.count
+      UseRight.update_counters u.id, :master_files_count => u.master_files.count
+    }
   end
 end
