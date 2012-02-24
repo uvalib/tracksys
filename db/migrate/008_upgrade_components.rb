48a4b45589c8cfb8171ea5ce82693fe3620289d3
Andrew Curley <andrew.curley@gmail.com>
2012-02-24 17:25:54 -0500

    Added component.date_ingested_into_dl and varios fields for relationship with UseRights

--- db/migrate/008_upgrade_components.rb
+++ db/migrate/008_upgrade_components.rb
@@ -2,15 +2,20 @@ class UpgradeComponents < ActiveRecord::Migration
   def change
     change_table(:components, :bulk => true) do |t|
       t.integer :availability_policy_id
+      t.datetime :date_ingested_into_dl
+      t.integer :use_right_id
+      t.integer :master_files_count, :default => 0, :null => false
       t.remove_index :name => 'bibl_id'
       t.remove_index :name => 'component_type_id'
       t.remove_index :name => 'indexing_scenario_id'
       t.index :component_type_id
       t.index :indexing_scenario_id
       t.index :availability_policy_id
+      t.index :use_right_id
       t.foreign_key :component_types
       t.foreign_key :indexing_scenarios
       t.foreign_key :availability_policies
+      t.foreign_key :use_rights
     end
   end
 end
