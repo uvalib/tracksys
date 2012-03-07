a98d892ba4b6deeee1ec8d32b10ab261a3495410
Andrew Curley <andrew.curley@gmail.com>
2012-03-06 21:32:06 -0500

    Beginning to flesh out Admin::Component

--- app/admin/components.rb
+++ app/admin/components.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register Component do
+
+  filter :component_type
+  filter :title
+  filter :content_desc
+  filter :pid
+
+  index do
+    column :title
+    column :date
+    column("Description") {|component| component.content_desc }
+    column :discoverability
+    column :date_ingested_into_dl
+    column :exemplar
+    default_actions
+  end
   
 end
