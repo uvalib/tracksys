6301f6201db90ff9e0aa86ffb351772da999cf1b
Andrew Curley <andrew.curley@gmail.com>
2012-03-06 21:33:38 -0500

    Provide links in Admin::IndexingScenario

--- app/admin/indexing_scenarios.rb
+++ app/admin/indexing_scenarios.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register IndexingScenario do
+  config.sort_order = 'name_asc'
+  
   menu :parent => "Miscellaneous"
 
   index do
@@ -6,6 +8,18 @@ ActiveAdmin.register IndexingScenario do
     column :pid
     column :datastream_name
     column :repository_url
+    column("Bibls") do |indexing_scenario|
+      link_to indexing_scenario.bibls_count.to_s, "bibls?q%5Bindexing_scenario_id_eq%5D=#{indexing_scenario.id}&order=title_asc"
+    end
+    column("Components") do |indexing_scenario|
+      link_to indexing_scenario.components_count.to_s, "components?q%5Bindexing_scenario_id_eq%5D=#{indexing_scenario.id}&order=name_asc"
+    end
+    column("Master Files") do |indexing_scenario|
+      link_to indexing_scenario.master_files_count.to_s, "master_files?q%5Bindexing_scenario_id_eq%5D=#{indexing_scenario.id}&order=id_asc"
+    end
+    column("Units") do |indexing_scenario|
+      link_to indexing_scenario.units_count.to_s, "units?q%5Bindexing_scenario_id_eq%5D=#{indexing_scenario.id}&order=id_asc"
+    end
     default_actions
   end
   
