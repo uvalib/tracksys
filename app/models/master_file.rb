292e5829af5e7bd5821ee1c1f829bd84ae56cf90
Andrew Curley <andrew.curley@gmail.com>
2012-03-06 21:36:01 -0500

    Created index_scope for MasterFile.  Think it improves speed of record return

--- app/models/master_file.rb
+++ app/models/master_file.rb
@@ -1,6 +1,9 @@
 require "#{Hydraulics.models_dir}/master_file"
 
 class MasterFile
+
+  scope :index_scope, select(["`master_files`.id", :filename, :title, :description, "`master_files`.date_ingested_into_dl", "`master_files`.date_archived", "`master_files`.pid"])
+  
   def link_to_thumbnail
     #master_file = MasterFile.find(params[:id])
     thumbnail_name = self.filename.gsub(/tif/, 'jpg')
