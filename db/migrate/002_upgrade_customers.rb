8652226586c6da6eca24de2188aff23d32c90641
Andrew Curley <andrew.curley@gmail.com>
2012-03-08 12:22:57 -0500

    Change to customer.billable_address

--- db/migrate/002_upgrade_customers.rb
+++ db/migrate/002_upgrade_customers.rb
@@ -20,7 +20,7 @@ class UpgradeCustomers < ActiveRecord::Migration
       c.build_primary_address(:address_1 => c.address_1.to_s, :address_2 => c.address_2.to_s, :city => c.city.to_s, :state => c.state.to_s, :country => c.country, :post_code => c.post_code.to_s, :phone => c.phone.to_s, :organization => c.organization.to_s)
 
       if c.billing_address
-        c.build_billing_address(:address_1 => c.billing_address.address_1.to_s, :address_2 => c.billing_address.address_2.to_s, :city => c.billing_address.city.to_s, :state => c.billing_address.state.to_s, :country => c.billing_address.country, :post_code => c.billing_address.post_code.to_s, :phone => c.billing_address.phone.to_s, :organization => c.billing_address.organization.to_s)
+        c.build_billable_address(:address_1 => c.billing_address.address_1.to_s, :address_2 => c.billing_address.address_2.to_s, :city => c.billing_address.city.to_s, :state => c.billing_address.state.to_s, :country => c.billing_address.country, :post_code => c.billing_address.post_code.to_s, :phone => c.billing_address.phone.to_s, :organization => c.billing_address.organization.to_s)
       end
       c.save!
     } 
