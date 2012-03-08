a96d0e73de80db1d3658fbf13a31c109000b547f
Andrew Curley <andrew.curley@gmail.com>
2012-03-08 11:19:42 -0500

    Remove mention of nonexistent agency.organization

--- db/migrate/010_upgrade_agencies.rb
+++ db/migrate/010_upgrade_agencies.rb
@@ -2,7 +2,7 @@ class UpgradeAgencies < ActiveRecord::Migration
   def change
 
     Agency.all.each {|a|
-      a.build_primary_address(:address_1 => a.address_1.to_s, :address_2 => a.address_2.to_s, :city => a.city.to_s, :state => a.state.to_s, :country => a.country, :post_code => a.post_code.to_s, :phone => a.phone.to_s, :organization => a.organization.to_s)
+      a.build_primary_address(:address_1 => a.address_1.to_s, :address_2 => a.address_2.to_s, :city => a.city.to_s, :state => a.state.to_s, :country => a.country, :post_code => a.post_code.to_s, :phone => a.phone.to_s)
     
       if a.billing_address
         a.build_billing_address(:address_1 => a.billing_address.address_1.to_s, :address_2 => a.billing_address.address_2.to_s, :city => a.billing_address.city.to_s, :state => a.billing_address.state.to_s, :country => a.billing_address.country, :post_code => a.billing_address.post_code.to_s, :phone => a.billing_address.phone.to_s, :organization => a.billing_address.organization.to_s)
@@ -18,7 +18,6 @@ class UpgradeAgencies < ActiveRecord::Migration
       t.remove :country
       t.remove :post_code
       t.remove :phone
-      t.remove :organization
       t.integer :orders_count, :default => 0
     end
   end
