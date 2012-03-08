9cbf040e08c83143a8ddc2a0da24542365a9cece
Andrew Curley <andrew.curley@gmail.com>
2012-03-08 12:07:11 -0500

    Had to alter Customer migration to handle local academic_status presence validation

--- db/migrate/002_upgrade_customers.rb
+++ db/migrate/002_upgrade_customers.rb
@@ -1,6 +1,21 @@
 class UpgradeCustomers < ActiveRecord::Migration
   def change
 
+    change_table(:customers, :bulk => true) do |t|
+      t.integer :master_files_count, :default => 0
+      t.remove_index :uva_status_id
+      t.rename :uva_status_id, :academic_status_id
+      t.index :academic_status_id
+      t.remove_index :name => 'department_id'
+      t.remove_index :name => 'heard_about_service_id'
+      t.index :department_id
+      t.index :heard_about_service_id
+      t.column :orders_count, :integer, :default => 0
+      t.foreign_key :academic_statuses
+      t.foreign_key :departments
+      t.foreign_key :heard_about_services
+    end
+
     Customer.all.each {|c|
       c.build_primary_address(:address_1 => c.address_1.to_s, :address_2 => c.address_2.to_s, :city => c.city.to_s, :state => c.state.to_s, :country => c.country, :post_code => c.post_code.to_s, :phone => c.phone.to_s, :organization => c.organization.to_s)
 
@@ -19,18 +34,6 @@ class UpgradeCustomers < ActiveRecord::Migration
       t.remove :post_code
       t.remove :phone
       t.remove :organization
-      t.integer :master_files_count, :default => 0
-      t.remove_index :uva_status_id
-      t.rename :uva_status_id, :academic_status_id
-      t.index :academic_status_id
-      t.remove_index :name => 'department_id'
-      t.remove_index :name => 'heard_about_service_id'
-      t.index :department_id
-      t.index :heard_about_service_id
-      t.column :orders_count, :integer, :default => 0
-      t.foreign_key :academic_statuses
-      t.foreign_key :departments
-      t.foreign_key :heard_about_services
     end
   end
 end
