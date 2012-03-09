cb6e074f7ec91538367957925cacd7a45ca657cf
Andrew Curley <andrew.curley@gmail.com>
2012-03-09 17:23:26 -0500

    Construct table for Patron:Dashboard Unit.awaiting_condition_approval

--- app/patron/dashboards.rb
+++ app/patron/dashboards.rb
@@ -63,6 +63,12 @@ ActiveAdmin::Dashboards.build do
   end
 
   section "Units Awaiting Condition Review", :namespace => :patron do
+    table_for Unit.awaiting_condition_approval do
+      column("Unit ID") {|unit| link_to unit.id, patron_unit_path(unit)}
+      column (:date_due){|unit| format_date(unit.order_date_due)}
+      column :bibl_title
+      column :bibl_call_number
+    end
   end
 
   section "Units Awaiting Copyright/IP Review", :namespace => :patron do
