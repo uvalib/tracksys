22030b4ea31b2dbfffef784196e029e87f8f96a6
Andrew Curley <andrew.curley@gmail.com>
2012-03-10 18:43:58 -0500

    Update AutomationMessage migration to deal with new messagable relationship

--- db/migrate/009_upgrade_automation_messages.rb
+++ db/migrate/009_upgrade_automation_messages.rb
@@ -19,5 +19,38 @@
     # TODO: Update all existing automation workflow_type based on processor name
     # AutomationMessage.find(:all).each {|am|
     # }
+
+    # Transition all legacy *_id to messagable_id and messagable_type
+    AutomationMessage.where('bibl_id is not null').find_each(:batch_size => 50000) do |am|
+      am.update_attribute(:messagable_type, "Bibl")
+      am.update_attribute(:messagable_id, am.bibl_id)
+    end
+
+    AutomationMessage.where('order_id is not null').find_each(:batch_size => 50000) do |am|
+      am.update_attribute(:messagable_type, "Order")
+      am.update_attribute(:messagable_id, am.order_id)
+    end
+
+    AutomationMessage.where('component_id is not null').find_each(:batch_size => 50000) do |am|
+      am.update_attribute(:messagable_type, "Component")
+      am.update_attribute(:messagable_id, am.component_id)
+    end
+
+    AutomationMessage.where('master_file_id is not null').find_each(:batch_size => 50000) do |am|
+      am.update_attribute(:messagable_type, "MasterFile")
+      am.update_attribute(:messagable_id, am.master_file_id)
+    end
+
+    AutomationMessage.where('unit_id is not null').find_each(:batch_size => 50000) do |am|
+      am.update_attribute(:messagable_type, "Unit")
+      am.update_attribute(:messagable_id, am.unit_id)
+    end
+
+    AutomationMessage.where('bibl_id is not null').update_all( :bibl_id => nil )
+    AutomationMessage.where('order_id is not null').update_all( :order_id => nil )
+    AutomationMessage.where('component_id is not null').update_all( :component_id => nil )
+    AutomationMessage.where('master_file_id is not null').update_all( :master_file_id => nil )
+    AutomationMessage.where('unit_id is not null').update_all( :unit_id => nil )
+
   end
 end
