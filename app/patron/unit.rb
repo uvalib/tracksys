1e933ae705bfd3caf268c2425b62996c87ca08ef
Andrew Curley <andrew.curley@gmail.com>
2012-03-13 11:38:32 -0400

    Initial commit of controller and member_action change_status in Patron:Unit

--- app/patron/unit.rb
+++ app/patron/unit.rb
@@ -15,7 +15,16 @@ ActiveAdmin.register Unit, :namespace => :patron do
   end
 
   controller do
+  member_action :change_status
 
+  controller do
+    include ActiveMessaging::MessageSender
+
+      message = ActiveSupport::JSON.encode( { :unit_id => params[:id] :unit_status => params[:unit_status] })
+      publish :update_unit_status, message
+      flash[:notice] = "Unit #{params[:id]} status has been changed to #{params[:unit_status]}"
+      redirect_to :back      
+    end
   end
 
 end
