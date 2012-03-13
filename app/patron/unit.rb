7d3c4e2724ade243bac8a38bf101b8b1a4a869ae
Andrew Curley <andrew.curley@gmail.com>
2012-03-13 11:38:51 -0400

    Some sidebar work for Patron:Unit

--- app/patron/unit.rb
+++ app/patron/unit.rb
@@ -13,13 +13,27 @@ ActiveAdmin.register Unit, :namespace => :patron do
   form do
 
   end
+  
+  sidebar :approval_workflow, :only => [:show] do
+    div :class => 'workflow_button' do
+      button_to "Approve", change_status_order_patron_order_path(order), :disabled => disabled, :method => 'get'
+    div :class => 'workflow_button' do
+      button_to "Cancel", change_status_order_patron_order_path(order), :disabled => disabled, :method => 'get'
+    end
+    div :class => 'workflow_button' do 
+      button_to "Send to Copyright Approval", change_status_order_patron_order_path(order), :disabled => disabled, :method => 'get'
+    end
+    div :class => 'workflow_button' do
+      button_to "Send to Condition Approval", change_status_order_patron_order_path(order), :disabled => disabled, :method => 'get'
+    end
+  end
 
-  controller do
   member_action :change_status
 
   controller do
     include ActiveMessaging::MessageSender
 
+    def change_status
       message = ActiveSupport::JSON.encode( { :unit_id => params[:id] :unit_status => params[:unit_status] })
       publish :update_unit_status, message
       flash[:notice] = "Unit #{params[:id]} status has been changed to #{params[:unit_status]}"
