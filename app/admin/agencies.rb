2e8e1a9bbcedcf96c7f450bc6a5ca5a634ce317c
Andrew Curley <andrew.curley@gmail.com>
2012-03-27 12:31:28 -0400

    Basic commit of Admin::Agencies index

--- app/admin/agencies.rb
+++ app/admin/agencies.rb
@@ -2,5 +2,10 @@ ActiveAdmin.register Agency do
   menu :parent => "Miscellaneous"
 
   scope :all, :default => true
+
+  index :id => 'agencies' do 
+    selectable_column
+    column :name
+  end
   
 end
