966f2395e997fa86e27c2f221e1652802cac8e8b
Andrew Curley <andrew.curley@gmail.com>
2012-04-20 16:31:44 -0400

    Add update method to ApplicationController to handle PUT requests from Oxygen.  Merge XML data with params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+   protect_from_forgery
+
+  def update
+    if env["HTTP_USER_AGENT"] =~ /Oxygen/ && env["REQUEST_METHOD"] == "PUT"
+      xml = Hash.from_xml(request.body.read)
+      params.merge!(xml)
+    end
+  end
 end
