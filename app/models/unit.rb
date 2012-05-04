f12aafc933553f954f6eae4bd65d8be8c598caab
Andrew Curley <andrew.curley@gmail.com>
2012-05-04 15:05:24 -0400

    Override Unit overdue_materials and checkedout_materials scopes
    Since the information for older units does not exist, we need to limit
    the scope searching to a date more proximate to the application's
    deployment.  This date will probably be pushed up again.

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,6 +1,10 @@
 require "#{Hydraulics.models_dir}/unit"
 
 class Unit
+  # Override Hydraulics Unit.overdue_materials and Unit.checkedout_materials scopes becuase our data should only concern itself 
+  # with those materials checkedout a few months before Tracksys 3 goes live (i.e. before March 1st?)
+  scope :overdue_materials, where("date_materials_received IS NOT NULL AND date_archived IS NOT NULL AND date_materials_returned IS NULL").where('date_materials_received >= "2012-03-01"')
+  scope :checkedout_materials, where("date_materials_received IS NOT NULL AND date_materials_returned IS NULL").where('date_materials_received >= "2012-03-01"')
 
   after_update :fix_updated_counters
 end
