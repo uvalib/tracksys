23ddaa3bc5ef1c2dbffb11957bff9de29a79e695
Andrew Curley <andrew.curley@gmail.com>
2012-05-04 14:29:56 -0400

    JS for toggling panels in show and edit view.

--- app/assets/javascripts/tracksys.js
+++ app/assets/javascripts/tracksys.js
@@ -1,6 +1,17 @@
+// Toggle show panels and form sections
 $(function(){
-  $('.panel[toggle] h3').live('click', function(e) {
-  $(e.target).next('.panel_contents').slideToggle("fast");
+  $('.panel[toggle] h3, fieldset[toggle] legend span').live('click', function(e) {
+    var $target = $(e.target);
+    if ($target.is('.panel[toggle] h3')) {
+      $target.next('.panel_contents').slideToggle("fast");
+      return false;
+    }
+    if ($target.is('span')) {
+      // alert($target.parent().next().is('ol'));
+      $target.parent().next('ol').slideToggle("fast");
+      return false;
+    }
+  $(e.target).next('.panel_contents', 'ol').slideToggle("fast");
     return false;
   });
 
