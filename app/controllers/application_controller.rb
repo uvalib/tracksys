91bdf8fca4a04294dc2fe19af2ddb46d6278bbe9
Andrew Curley <andrew.curley@gmail.com>
2012-05-04 14:59:11 -0400

    Reinsert protect_from_forgery on ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-   protect_from_forgery
+  protect_from_forgery
 
   def update
     if env["HTTP_USER_AGENT"] =~ /Oxygen/ && env["REQUEST_METHOD"] == "PUT"
