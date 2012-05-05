7de921ee7b2b8c4d48bde20458043d2b9d5f0767
Andrew Curley <andrew.curley@gmail.com>
2012-05-04 20:31:36 -0400

    Create default Devise user.

--- db/migrate/20110928202329_devise_create_admin_users.rb
+++ db/migrate/20110928202329_devise_create_admin_users.rb
@@ -41,6 +41,9 @@ class AddDeviseToAdminUsers < ActiveRecord::Migration
       # t.timestamps
     end
 
+    # Create a default user
+    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+
     add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
     # add_index :admin_users, :confirmation_token,   :unique => true
