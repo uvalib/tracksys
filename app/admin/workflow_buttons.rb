0dbe97473073dcd5a235b66676bdb3f3f7f1227b
Andrew Curley <andrew.curley@gmail.com>
2012-05-22 15:51:12 -0400

    Totally revamp the articial page Admin::WorkflowButton which holds resourceless workflow step

--- app/admin/workflow_buttons.rb
+++ app/admin/workflow_buttons.rb
@@ -1,49 +1,48 @@
 ActiveAdmin.register_page "WorkflowStart" do
-  menu :label => 'Workflow Buttons', :parent => "Dashboard"
+  menu :label => 'Workflow Buttons', :parent => "Miscellaneous"
+
+  content do
+    panel "Buttons" do
+      div :class => 'workflow_button' do button_to "Start Finalization on digiserv-production", admin_workflow_start_start_finalization_production_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+      div :class => 'workflow_button' do button_to "Start Finalization on digiserv-migration", admin_workflow_start_start_finalization_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+      div :class => 'workflow_button' do button_to "Start Manual Stornext Upload on digiserv-prodution", admin_workflow_start_start_manual_upload_to_archive_production_path, :method => :get end
+      div :class => 'workflow_button' do button_to "Start Manual Stornext Upload on digiserv-migration", admin_workflow_start_start_manual_upload_to_archive_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+    end
+  end
 
   page_action :start_finalization_production do
-    message = ActiveSupport::JSON.encode( {:message => 'go'})
+    message = ActiveSupport::JSON.encode( {:user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s)} )
     publish :start_finalization_production, message
     flash[:notice] = "Items in /digiserv-production/finalization/10_dropoff have begun finalization workflow."
     redirect_to :back
   end
 
-
   page_action :start_finalization_migration do
-    message = ActiveSupport::JSON.encode( {:message => 'go'})
+    message = ActiveSupport::JSON.encode( {:user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s)} )
     publish :start_finalization_migration, message
     flash[:notice] = "Items in /digiserv-migration/finalization/10_dropoff have begun finalization workflow."
     redirect_to :back
   end
 
   page_action :start_manual_upload_to_archive_production do
-    message = ActiveSupport::JSON.encode( {:message => 'go'})
+    @user = StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s)
+    message = ActiveSupport::JSON.encode( {:user_id => @user.id } )
     publish :start_manual_upload_to_archive_production, message
     flash[:notice] = "Items in /digiserv-production/stornext_dropoff/#{Time.now.strftime('%A')}."
     redirect_to :back
   end
 
   page_action :start_manual_upload_to_archive_migration do
-    message = ActiveSupport::JSON.encode( {:message => 'go'})
+    message = ActiveSupport::JSON.encode( {:user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s)} )
     publish :start_manual_upload_to_archive_migration, message
     flash[:notice] = "Items in /digiserv-migration/stornext_dropoff/#{Time.now.strftime('%A')}."
     redirect_to :back
   end
 
-  content do
-    div do button_to "Start Finalization on digiserv-production", admin_workflow_start_start_finalization_production_url end
-    div do button_to "Start Finalization on digiserv-migration", admin_workflow_start_start_finalization_migration_url end
-    div do button_to "Start Manual Stornext Upload on digiserv-prodution", admin_workflow_start_start_manual_upload_to_archive_production_url end
-    div do button_to "Start Manual Stornext Upload on digiserv-migration", admin_workflow_start_start_manual_upload_to_archive_migration_url end
+  controller do 
+    require 'activemessaging/processor'
+    include ActiveMessaging::MessageSender
+
+    publishes_to :start_manual_upload_to_archive_production
   end
-
-#   controller do
-# def start_finalization_production
-#     message = ActiveSupport::JSON.encode( {:message => 'go'})
-#     publish :start_finalization_production, message
-#     flash[:notice] = "Items in /digiserv-production/finalization/10_dropoff have begun finalization workflow."
-#     redirect_to :back
-#   end
-#   end
-
 end
