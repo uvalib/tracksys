0de465a7156a3af17998637d4463c11bf3b52da0
Andrew Curley <andrew.curley@gmail.com>
2012-05-22 16:02:03 -0400

    Serialize email, add two new scopes for separating FineArts based content and add workflow methods to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,12 @@
 require "#{Hydraulics.models_dir}/order"
 
 class Order
+  serialize :email
+
   after_update :fix_updated_counters
+
+  scope :from_fine_arts, joins(:agency).where("agencies.name" => "Fine Arts Library")
+  scope :not_from_fine_arts, where('agency_id != 37 or agency_id is null')
 
   # Processor information
   require 'activemessaging/processor'
@@ -17,17 +22,20 @@ class Order
     publish :update_order_status_canceled, message
   end
 
-  def send_order_email
+  def check_order_ready_for_delivery
     message = ActiveSupport::JSON.encode( {:order_id => self.id})
-    publish :update_order_email_date, message
+    publish :check_order_ready_for_delivery, message
   end
 
-  # def check_order_ready_for_delivery
-  #   message = ActiveSupport::JSON.encode( {:order_id => self.id})
-  #   publish :check_order_ready_for_delivery, message
-  #   flash[:notice] = "Workflow started at checking the completeness of the order."
-  #   redirect_to admin_order_path
-  # end
+  def create_order_pdf
+    message = ActiveSupport::JSON.encode( {:order_id => self.id, :fee => self.fee_actual.to_i})
+    publish :create_order_pdf, message
+  end
+
+  def qa_order_data
+    message = ActiveSupport::JSON.encode({:order_id => self.id})
+    publish :qa_order_data, message
+  end
 
   def send_fee_estimate_to_customer(computing_id)
     @user = StaffMember.find_by_computing_id(computing_id) 
@@ -35,4 +43,9 @@ class Order
     message = ActiveSupport::JSON.encode( {:order_id => self.id, :first_name => @first_name})
     publish :send_fee_estimate_to_customer, message
   end
+
+  def send_order_email
+    message = ActiveSupport::JSON.encode( {:order_id => self.id})
+    publish :send_order_email, message
+  end  # End processor methods
 end
