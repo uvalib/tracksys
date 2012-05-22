a0c21b13eb532d533682e3f98654553af73597c1
Andrew Curley <andrew.curley@gmail.com>
2012-05-22 16:44:18 -0400

    Totally revamp Admin::Dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -44,40 +44,16 @@ ActiveAdmin::Dashboards.build do
       column ("Thumbnail") {|bibl| image_tag("http://fedoraproxy.lib.virginia.edu/fedora/get/#{MasterFile.find_by_filename(bibl.exemplar).pid}/djatoka:jp2SDef/getRegion?scale=125", :height => '100')}
       column("Links") do |mf|
         div do
-          link_to "Details", admin_master_file_path(mf), :class => "member_link view_link"
+          link_to "Details", admin_bibl_path(mf), :class => "member_link view_link"
         end
       end
-      # column ("") do |bibl|
-      #   div do
-      #     tweet_button(:via => 'UVaDigServ', :url => "http://search.lib.virginia.edu/catalog/#{bibl.pid}", :text => truncate("#{bibl.title}", :length => 80), :count => 'horizontal')
-      #   end
-      #   div do
-      #     link_to "Pin It", "http://pinterest.com/pin/create/button/?#{URI.encode_www_form("url" => "http://search.lib.virginia.edu/catalog/#{bibl.pid}/view", "media" => "http://fedoraproxy.lib.virginia.edu/fedora/get/#{MasterFile.find_by_filename(bibl.exemplar).pid}/djatoka:jp2SDef/getRegion?scale=800", "description" => "#{bibl.title} &#183; #{bibl.creator_name} &#183; #{bibl.year} &#183; Albert and Shirley Small Special Collections Library, University of Virginia.")}", :class => "pin-it-button"
-      #   end
-      # end
     end
   end
 
   section "Buttons" do
-     div do button_to "Start Finalization on digiserv-production", admin_workflow_start_start_finalization_production_url end
-    div do button_to "Start Finalization on digiserv-migration", admin_workflow_start_start_finalization_migration_url end
-    div do button_to "Start Manual Stornext Upload on digiserv-prodution", admin_workflow_start_start_manual_upload_to_archive_production_url end
-    div do button_to "Start Manual Stornext Upload on digiserv-migration", admin_workflow_start_start_manual_upload_to_archive_migration_url end
+    div :class => 'workflow_button' do button_to "Start Finalization on digiserv-production", admin_workflow_start_start_finalization_production_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+    div :class => 'workflow_button' do button_to "Start Finalization on digiserv-migration", admin_workflow_start_start_finalization_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+    div :class => 'workflow_button' do button_to "Start Manual Stornext Upload on digiserv-prodution", admin_workflow_start_start_manual_upload_to_archive_production_path, :method => :get end
+    div :class => 'workflow_button' do button_to "Start Manual Stornext Upload on digiserv-migration", admin_workflow_start_start_manual_upload_to_archive_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
   end
-
-  # section "Recently Ingested into DL" do
-  #   table_for Unit.in_repo.limit(30) do
-  #     column("Call Number") {|unit| unit.bibl_call_number}
-  #     column("Title") {|unit| truncate(unit.bibl_title, :length => 80)}
-  #     column ("Thumbnail") {|unit| image_tag("http://fedoraproxy.lib.virginia.edu/fedora/get/#{MasterFile.find_by_filename(unit.bibl_exemplar).pid}/djatoka:jp2SDef/getRegion?scale=125", :height => '100')}
-  #     column ("") do |unit|
-  #       div do
-  #         tweet_button(:via => 'UVaDigServ', :url => "http://search.lib.virginia.edu/catalog/#{unit.bibl_pid}", :text => truncate("#{unit.bibl_title}", :length => 80), :count => 'horizontal')
-  #       end
-  #       div do
-  #         link_to "Pin It", "http://pinterest.com/pin/create/button/?#{URI.encode_www_form("url" => "http://search.lib.virginia.edu/catalog/#{unit.bibl_pid}/view", "media" => "http://fedoraproxy.lib.virginia.edu/fedora/get/#{MasterFile.find_by_filename(unit.bibl_exemplar).pid}/djatoka:jp2SDef/getRegion?scale=800", "description" => "#{unit.bibl_title} \n\n Albert and Shirley Small Special Collections Library, University of Virginia.")}", :class => "pin-it-button"
-  #       end
-  #     end
-  #   end
-  # end
 end
