3dd7bb75bf43ae4ae40d79f7bbd89f689a4a47d5
Andrew Curley <andrew.curley@gmail.com>
2012-05-22 15:56:53 -0400

    Add Piddable module to Bibl

--- app/models/bibl.rb
+++ app/models/bibl.rb
@@ -2,9 +2,11 @@ require "#{Hydraulics.models_dir}/bibl"
 
 class Bibl
 
-  VIRGO_FIELDS = ['title', 'creator_name', 'creator_name_type', 'call_number', 'catalog_key', 'barcode', 'copy', 'date_external_update', 'location', 'citation', 'year', 'year_type', 'location', 'copy', 'title_control']
+  include Pidable
 
-  after_update :fix_updated_counters
+  VIRGO_FIELDS = ['title', 'creator_name', 'creator_name_type', 'call_number', 'catalog_key', 'barcode', 'copy', 'date_external_update', 'location', 'citation', 'year', 'year_type', 'location', 'copy', 'title_control', 'date_external_update']
+
+#  after_update :fix_updated_counters
 
   #------------------------------------------------------------------
   # aliases
