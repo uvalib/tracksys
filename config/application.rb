3eb13f86f163703a8a3899eefc1abdd346b5c553
Andrew Curley <andrew.curley@gmail.com>
2012-06-02 18:41:53 -0400

    Add assets/images subdirectory to assets path

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,7 @@ module Tracksys
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.paths << "#{config.root}/assets/images/request_form"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
