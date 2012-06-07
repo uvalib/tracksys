9a31606b69f442555c668275e7439de7ce6b53fc
Andrew Curley <andrew.curley@gmail.com>
2012-06-07 15:08:15 -0400

    Changed datepicker dateFormat to comply with what rails is looking for.

--- app/assets/javascripts/request_form.js
+++ app/assets/javascripts/request_form.js
@@ -3,6 +3,7 @@ $(function() {
     minDate: "+28d",
     beforeShowDay: $.datepicker.noWeekends,
     numberOfMonths: 3,
+    dateFormat: "yy-mm-dd",
     showOptions: {direction: 'down'}
   });
 });
