dbd748e75770a9f2d97d909dde869b838d0d78c2
Andrew Curley <andrew.curley@gmail.com>
2012-06-13 17:13:58 -0400

    Create two new scopes for approved HeardAboutService and HeardAboutResource to be used in request form.

--- app/models/heard_about_resource.rb
+++ app/models/heard_about_resource.rb
@@ -2,7 +2,8 @@ require "#{Hydraulics.models_dir}/heard_about_resource"
 
 class HeardAboutResource
   default_scope :order => :description
-
+  scope :for_request_form, where(:is_approved => true).where(:is_internal_use_only => false)
+  
   #------------------------------------------------------------------
   # aliases
   #------------------------------------------------------------------
--- app/models/heard_about_service.rb
+++ app/models/heard_about_service.rb
@@ -2,4 +2,5 @@ require "#{Hydraulics.models_dir}/heard_about_service"
 
 class HeardAboutService
   default_scope :order => :description
+  scope :for_request_form, where(:is_approved => true).where(:is_internal_use_only => false)
 end
