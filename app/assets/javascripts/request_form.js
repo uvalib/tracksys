ca66115305a9ab02c95ed5cc7c8388627ab31e65
Andrew Curley <andrew.curley@gmail.com>
2012-06-13 17:14:31 -0400

    Added deliverable image information via javascript tied to a nested_form event handler.

--- app/assets/javascripts/request_form.js
+++ app/assets/javascripts/request_form.js
@@ -7,3 +7,20 @@ $(function() {
     showOptions: {direction: 'down'}
   });
 });
+
+$("form").live("nested:fieldAdded", function(event) {
+  $('.intended_use_select').change(function() {
+    var intended_use_id = $(this).closest('.intended_use_select').val();
+    // JPEG Intended Use Values
+    if(intended_use_id == 100 || intended_use_id == 103 || intended_use_id == 104 || intended_use_id == 106 || intended_use_id == 109 || intended_use_id == 111) {
+      $(this).parent().parent().siblings('#intended_use_watermarked_jpg').attr("style","display: block;");
+      $(this).parent().parent().siblings('#intended_use_highest_tif').attr("style","display: none;");
+    }
+    // TIF Intended Use Values
+    if(intended_use_id == 101 || intended_use_id == 102 || intended_use_id == 105 || intended_use_id == 107 || intended_use_id == 108 || intended_use_id == 110) {
+      $(this).parent().parent().siblings('#intended_use_watermarked_jpg').attr("style","display: none;");
+      $(this).parent().parent().siblings('#intended_use_highest_tif').attr("style","display: block;");
+    }
+  });
+});
+
