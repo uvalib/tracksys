c69d39b2d9516d54054b444667b1ebccec345ee8
Andrew Curley <andrew.curley@gmail.com>
2012-06-22 13:49:27 -0400

    Alter ImageTechMeta to provide method for mime_type

--- app/models/image_tech_meta.rb
+++ app/models/image_tech_meta.rb
@@ -1,4 +1,29 @@
 require "#{Hydraulics.models_dir}/image_tech_meta"
 
 class ImageTechMeta
+   def mime_type
+    if format.blank?
+      return nil
+    else
+      # image formats
+      if format.match(/^(gif|jpeg|tiff)$/i)
+        return "image/#{format.downcase}"
+      elsif format.match(/^mrsid$/i)
+        return "image/x-mrsid"
+      elsif format.match(/^jpeg ?2000$/i)
+        return 'image/jp2'
+      # text formats
+      elsif format == 'TEI-XML'
+        return 'text/xml'
+      # audio formats
+      elsif format == 'WAV'
+        return 'audio/wav'
+      # video formats
+      elsif format == 'AVI'
+        return 'video/avi'
+      else
+        return nil
+      end
+    end
+  end
 end
