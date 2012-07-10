c9201f2881982116f6084c1749b32cbc4788995c
Andrew Curley <andrew.curley@gmail.com>
2012-07-10 17:24:06 -0400

    New fields for Component table

---
+++ db/migrate/20120710200935_add_fields_to_componentsa.rb
@@ -0,0 +1,10 @@
+class AddFieldsToComponentsa < ActiveRecord::Migration
+  def change
+  	change_table(:components, :bulk => true) do |t|
+      t.integer :followed_by_id
+      t.index :followed_by_id
+      t.text :legacy_ead
+      t.text :physical_desc
+  	end
+  end
+end
