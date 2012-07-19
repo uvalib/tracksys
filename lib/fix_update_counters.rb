6f5f078bae6662e5fa5cc7aa343228884a9f07db
Andrew Curley <andrew.curley@gmail.com>
2012-07-19 12:00:28 -0400

    Remove logging statement in FixUpdateCounters

--- lib/fix_update_counters.rb
+++ lib/fix_update_counters.rb
@@ -6,7 +6,6 @@ module FixUpdateCounters
       # value should be an array ['old value', 'new value']
       if key =~ /_id/
         changed_class = key.sub(/_id/, '')
-        logger.info "#{self.class.name.downcase.pluralize}"
         changed_class.camelcase.constantize.decrement_counter(:"#{self.class.name.underscore.pluralize}_count", value[0]) unless value[0] == nil
         changed_class.camelcase.constantize.increment_counter(:"#{self.class.name.underscore.pluralize}_count", value[1]) unless value[1] == nil
       end
