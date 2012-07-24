97a6fd5de7a51313f3348f10e03c31d10a600ec4
Matthew Stephens <ms3uf@Virginia.edu>
2012-07-24 17:29:38 -0400

    adding rainbow class to components

--- app/admin/components.rb
+++ app/admin/components.rb
@@ -74,11 +74,24 @@ ActiveAdmin.register Component do
               "Unknown"
             end
           end
-          row(:desc_metadata) {|component| truncate_words(component.desc_metadata)}
+          row(:desc_metadata) {|component| 
+            if component.desc_metadata
+              pre :class => "no-whitespace" do 
+                code :'data-language' => 'html' do
+                  word_wrap(component.desc_metadata.to_s, :line_width => 80)
+                end
+              end
+            end
+          }
           row(:solr) {|component| truncate_words(component.solr)}
           row(:dc) {|component| truncate_words(component.dc)}
           row(:rels_ext) {|component| truncate_words(component.rels_ext)}
           row(:rels_int) {|component| truncate_words(component.rels_int)}
+          row(:legacy_ead) {|component| 
+            if component.legacy_ead
+              pre do code :'data-language' => 'html' do component.legacy_ead.to_s end end
+            end
+          }
         end
       end
     end
@@ -171,6 +184,7 @@ ActiveAdmin.register Component do
       f.input :dc, :input_html => {:rows => 5}
       f.input :rels_ext, :input_html => {:rows => 5}
       f.input :rels_int, :input_html => {:rows => 5}
+      f.input :legacy_ead, :input_html => {:rows => 5}
     end
 
     f.inputs :class => 'columns-none' do
