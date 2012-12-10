bdb9bd6f99ba2f85b95d7d8eeb1b96f7f4b37a75
Andrew Curley <andrew.curley@gmail.com>
2012-12-10 11:47:29 -0500

    Remove workflow buttons as a class and fold all actions into the Admin:Dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -49,12 +49,12 @@ ActiveAdmin.register_page "Dashboard" do
     end
 
     div :class => 'three-column' do 
-      panel "Buttons", :width => '33%', :priority => 3, :namespace => :admin, :toggle => 'show' do
-        div :class => 'workflow_button' do button_to "Finalize digiserv-production", admin_workflow_start_start_finalization_production_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
-        div :class => 'workflow_button' do button_to "Finalize digiserv-migration", admin_workflow_start_start_finalization_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
-        div :class => 'workflow_button' do button_to "Manual Upload digiserv-prodution", admin_workflow_start_start_manual_upload_to_archive_production_path, :method => :get end
-        div :class => 'workflow_button' do button_to "Manual Upload digiserv-migration", admin_workflow_start_start_manual_upload_to_archive_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
-        div :class => 'workflow_button' do button_to "Manual Upload lib_content37", admin_workflow_start_start_manual_upload_to_archive_batch_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+      panel "Finalization Workflow Buttons", :width => '33%', :priority => 3, :namespace => :admin, :toggle => 'show' do
+        div :class => 'workflow_button' do button_to "Finalize digiserv-production", admin_dashboard_start_finalization_production_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+        div :class => 'workflow_button' do button_to "Finalize digiserv-migration", admin_dashboard_start_finalization_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+        div :class => 'workflow_button' do button_to "Manual Upload digiserv-prodution", admin_dashboard_start_manual_upload_to_archive_production_path, :method => :get end
+        div :class => 'workflow_button' do button_to "Manual Upload digiserv-migration", admin_dashboard_start_manual_upload_to_archive_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+        div :class => 'workflow_button' do button_to "Manual Upload lib_content37", admin_dashboard_start_manual_upload_to_archive_batch_migration_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
       end
     end
 
@@ -74,18 +74,79 @@ ActiveAdmin.register_page "Dashboard" do
     end
 
     div :class => 'three-column' do
-      panel "Digital Library Actions", :priority => 5, :toggle => 'show' do
-        div :class => 'workflow_button' do button_to "Commit Records to Solr", admin_workflow_start_update_all_solr_docs_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
-        div :class => 'workflow_button' do button_to "Make Records Available on Virgo", admin_workflow_start_push_staging_to_production_path, :method => :get end
+      panel "Digital Library Buttons", :priority => 5, :toggle => 'show' do
+        div :class => 'workflow_button' do button_to "Commit Records to Solr", admin_dashboard_update_all_solr_docs_path, :user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s), :method => :get end
+        div :class => 'workflow_button' do button_to "Make Records Available on Virgo", admin_dashboard_push_staging_to_production_path, :method => :get end
       end
     end
 
     div :class => 'three-column' do
       panel "Statistics", :priority => 6, :toggle => 'show' do
         div do
-          render 'admin/workflow_start/stats_report'
+          render 'admin/stats_report'
         end
       end
     end
+  end
+
+  page_action :get_yearly_stats do 
+    message = ActiveSupport::JSON.encode( {:year => params[:year]})
+    publish :create_stats_report, message
+    flash[:notice] = "Stats Report Being Created.  Find at /digiserv-production/administrative/stats_report.  Give three minutes for production."
+    redirect_to :back
+  end
+
+  page_action :push_staging_to_production do 
+    begin
+      FileUtils.touch '/lib_content27/tracksys_prod_solr/copy_to_production'
+      flash[:notice] = "Records will be available through Virgo at 6am."
+    rescue Errno::EACCES
+      flash[:notice] = "Something went wrong, contact Administrator."
+    end
+    redirect_to :back
+  end
+
+  page_action :start_finalization_production do
+    message = ActiveSupport::JSON.encode( {:user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s)} )
+    publish :start_finalization_production, message
+    flash[:notice] = "Items in /digiserv-production/finalization/10_dropoff have begun finalization workflow."
+    redirect_to :back
+  end
+
+  page_action :start_finalization_migration do
+    message = ActiveSupport::JSON.encode( {:user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s)} )
+    publish :start_finalization_migration, message
+    flash[:notice] = "Items in /digiserv-migration/finalization/10_dropoff have begun finalization workflow."
+    redirect_to :back
+  end
+
+  page_action :start_manual_upload_to_archive_batch_migration do
+    @user = StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s)
+    message = ActiveSupport::JSON.encode( {:user_id => @user.id } )
+    publish :start_manual_upload_to_archive_batch_migration, message
+    flash[:notice] = "Items in /lib_content37/Rimage/stornext_dropoff/#{Time.now.strftime('%A')}."
+    redirect_to :back
+  end
+
+  page_action :start_manual_upload_to_archive_production do
+    @user = StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s)
+    message = ActiveSupport::JSON.encode( {:user_id => @user.id } )
+    publish :start_manual_upload_to_archive_production, message
+    flash[:notice] = "Items in /digiserv-production/stornext_dropoff/#{Time.now.strftime('%A')}."
+    redirect_to :back
+  end
+
+  page_action :start_manual_upload_to_archive_migration do
+    message = ActiveSupport::JSON.encode( {:user => StaffMember.find_by_computing_id(request.env['HTTP_REMOTE_USER'].to_s)} )
+    publish :start_manual_upload_to_archive_migration, message
+    flash[:notice] = "Items in /digiserv-migration/stornext_dropoff/#{Time.now.strftime('%A')}."
+    redirect_to :back
+  end
+
+  page_action :update_all_solr_docs do
+    message = ActiveSupport::JSON.encode( {:message => 'go'})
+    publish :send_commit_to_solr, message
+    flash[:notice] = "All Solr records have been committed to #{STAGING_SOLR_URL}."
+    redirect_to :back
   end
 end
