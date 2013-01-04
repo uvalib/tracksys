521e9f98f70be6bc9cadf2561d1a1b35db34b7b0
Andrew Curley <andrew.curley@gmail.com>
2013-01-04 18:46:46 -0500

    Remove incorrect add_index migration

--- db/migrate/20130104221035_change_bibl_title_from_text_to_string.rb
+++ db/migrate/20130104221035_change_bibl_title_from_text_to_string.rb
@@ -2,6 +2,5 @@ class ChangeBiblTitleFromTextToString < ActiveRecord::Migration
   def change
     remove_index :bibls, :title
     change_column :bibls, :title, :text
-    add_index :bibls, :title
  end
 end
