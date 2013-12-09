887a80b8df9f14e281bc4f7b1093cec9e1c8d114
Matthew Stephens <mattbastard@gmail.com>
2013-12-09 14:14:25 -0500

    all 14 tests passing

--- spec/lib/virgo_spec.rb
+++ spec/lib/virgo_spec.rb
@@ -23,12 +23,16 @@ describe Virgo do
     it "should take a catalog key and return the corresponding catalog record as a Bibl object" do
       cat_key ="u126747" # Corks and curls
       bibl = Virgo.external_lookup(cat_key, '')
-      expect(bibl.call_number).to eq "LD 5687 .C7 v.37 1924"
+      expect(bibl.title).to eq "Corks and curls"
+      expect(bibl.year).to eq "1888-"
+
     end
     it "should take a barcode and return the corresponding catalog record as a Bibl object" do
       barcode = "X001892622" # Corks and curls 
       bibl = Virgo.external_lookup('', barcode)
       expect(bibl.call_number).to eq "LD 5687 .C7 v.37 1924"
+      expect(bibl.title).to eq "Corks and curls"
+      expect(bibl.year).to eq "1888-"
     end
     # existing, short record
     it "should take a catalog key and return the corresponding catalog record as a Bibl object" do
@@ -44,11 +48,11 @@ describe Virgo do
     # non-existent id passed to method
     it "should take an invalid catalog key and raise an error" do
       bad_cat_key = "now_way_this_is_a_cat_key"
-      expect{ Virgo.external_lookup(bad_cat_key, '') }.to raise_error RuntimeError, "No corresponding record was found in the Library catalog"
+      expect{ Virgo.external_lookup(bad_cat_key, '') }.to raise_error RuntimeError, "Query to index.lib.virginia.edu failed to return a valid result."
     end
     it "should take an invalid barcode and raise an error" do
       bad_barcode = "no_way_this_is_a_barcode"
-      expect{ Virgo.external_lookup('', bad_barcode) }.to raise_error RuntimeError, "No corresponding record was found in the Library catalog"
+      expect{ Virgo.external_lookup('', bad_barcode) }.to raise_error RuntimeError, "Query to index.lib.virginia.edu failed to return a valid result."
     end
   end
   describe "#external_update" do
