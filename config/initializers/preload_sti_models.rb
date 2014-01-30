790b57df51e2809c57253dc77288e341538b60f1
Matthew Stephens <mattbastard@gmail.com>
2014-01-29 19:42:21 -0500

    restoring subclass deleted from preloader

--- config/initializers/preload_sti_models.rb
+++ config/initializers/preload_sti_models.rb
@@ -1,5 +1,5 @@
 if Rails.env.development?
-  %w[master_file jpeg_two_thousand].each do |c|
+  %w[master_file jpeg_two_thousand tiff].each do |c|
     require_dependency File.join(Rails.root, "app","models","#{c}.rb")
   end
 end
