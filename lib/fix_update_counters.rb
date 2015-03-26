c98e1a75527c650578c8302f9fc37df37fff8ba4
Steve Majewski <sdm7g@virginia.edu>
2015-03-26 19:20:50 -0400

    an attempt to fix callback problem with save!

--- lib/fix_update_counters.rb
+++ lib/fix_update_counters.rb
@@ -4,7 +4,7 @@ module FixUpdateCounters
     self.changes.each {|key, value|
       # key should match /master_files_id/ or /bibls_id/
       # value should be an array ['old value', 'new value']
-      if key =~ /_id/
+      if key =~ /_id/ and not ( key =~ /parent_bibl_id/ )
         changed_class = key.sub(/_id/, '')
         changed_class.camelcase.constantize.decrement_counter(:"#{self.class.name.underscore.pluralize}_count", value[0]) unless value[0] == nil
         changed_class.camelcase.constantize.increment_counter(:"#{self.class.name.underscore.pluralize}_count", value[1]) unless value[1] == nil
