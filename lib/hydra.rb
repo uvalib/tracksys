984e6ff56e88ac010faf71cfa6de5f1a1cd7b1ae
Steve Majewski <sdm7g@virginia.edu>
2015-05-26 16:31:45 -0400

    change remaining 'virgobeta' URLs to point to core (missed these when I updated virgo.rb )

--- lib/hydra.rb
+++ lib/hydra.rb
@@ -71,7 +71,7 @@ module Hydra
       # total_description = total_description.gsub(/\r/, ' ').gsub(/\n/, ' ').gsub(/\t/, ' ').gsub(/(  )+/, ' ')
       # total_title = total_title.gsub(/\r/, ' ').gsub(/\n/, ' ').gsub(/\t/, ' ').gsub(/(  )+/, ' ')
 
-      analog_solr_record = "http://#{SOLR_PRODUCTION_NAME}/virgobeta/select?q=id%3A#{object.catalog_key}"
+      analog_solr_record = "http://#{SOLR_PRODUCTION_NAME}/core/select?q=id%3A#{object.catalog_key}"
 
       index_destination = "localhost"
       if object.index_destination.respond_to?(:name)
@@ -118,7 +118,7 @@ module Hydra
       external_relations = "#{FEDORA_REST_URL}/objects/#{object.pid}/datastreams/RELS-EXT/content"
       external_relations = external_relations.gsub(/\r/, ' ').gsub(/\n/, ' ').gsub(/\t/, ' ').gsub(/(  )+/, ' ')
 
-      analog_solr_record = "http://#{SOLR_PRODUCTION_NAME}/solr/all/select?q=id%3A#{object.bibl.catalog_key}"
+      analog_solr_record = "http://#{SOLR_PRODUCTION_NAME}/solr/core/select?q=id%3A#{object.bibl.catalog_key}"
       total_transcription = object.transcription_text.gsub(/\r/, ' ').gsub(/\n/, ' ').gsub(/\t/, ' ').gsub(/(  )+/, ' ') unless object.transcription_text.blank?
       total_description = object.description.gsub(/\r/, ' ').gsub(/\n/, ' ').gsub(/\t/, ' ').gsub(/(  )+/, ' ') unless object.description.blank?
       total_title = object.title.gsub(/\r/, ' ').gsub(/\n/, ' ').gsub(/\t/, ' ').gsub(/(  )+/, ' ') unless object.title.blank?
