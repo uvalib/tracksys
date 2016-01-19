6b05f486c4e0aa596bc84b4b90cdea35f0827db6
lf6f <lf6f@UL-lf6f-mbpro.local>
2016-01-19 13:57:54 -0500

    error handling fixes

--- app/jobs/base_job.rb
+++ app/jobs/base_job.rb
@@ -11,7 +11,11 @@ class BaseJob
    #
    def self.exec_now(message)
       job = self.new()
-      job.perform(message)
+      begin
+         job.perform(message)
+      rescue Exception => e
+         job.on_error(e)
+      end
    end
 
    # Helper to make job names look like the original processor names
@@ -20,24 +24,46 @@ class BaseJob
      @workflow_type = AutomationMessage::WORKFLOW_TYPES_HASH.fetch("#{self.class.name.demodulize}Processor")
    end
 
+   # Delayed job error hook. Called when an exception is raised in a running job
+   # Pass it along to the tracksys error handling
+   #
+   def error(job, exception)
+      on_error(exception)
+   end
+
    def on_success(message)
       msg = {
          :messagable_id => @messagable_id, :messagable_type => @messagable_type,
          :pid => @pid, :message_type => 'success', :app => 'tracksys',
          :processor => self.class.name.demodulize, :workflow_type => @workflow_type, :message => message}
       Job_Log.info( message )
-      write_automation_message(msg)
+      BaseJob.write_automation_message(msg)
    end
 
+   # Sends a warning message, where "warning" is defined as a message that you
+   # want to log without terminating processing. When you call this method like
+   # so:
+   #   on_warning "My message"
+   # the message is logged, but then your code continues to run.
+   #
    def on_failure(message)
       msg = {
          :messagable_id => @messagable_id, :messagable_type => @messagable_type,
          :pid => @pid, :message_type => 'failure', :app => 'tracksys', :processor => self.class.name.demodulize,
          :workflow_type => @workflow_type, :message => message}
       Job_Log.error(message)
-      write_automation_message(msg)
+      BaseJob.write_automation_message(msg)
    end
 
+   # This method is called whenever an exception is raised within on_message.
+   # Handles StandardError exceptions and keeps processing. Other exceptions
+   # are raised, terminating processing.
+   #
+   # Have on_error throw ActiveMessaging::AbortMessageException when you want a
+   # message to be aborted/rolled back, meaning that it can and should be
+   # retried (idempotency matters here). Retry logic varies by broker - see
+   # individual adapter code and docs for how it will be treated
+   #
    def on_error(err)
       if err.is_a? StandardError
          msg = {
@@ -45,16 +71,15 @@ class BaseJob
             :pid => @pid, :message_type => 'error', :app => 'tracksys', :processor => self.class.name.demodulize,
             :workflow_type => @workflow_type, :message => err.message, :class_name => err.class.name,
             :backtrace => err.backtrace.join("\n")}
-         Job_Log.error(err)
-         write_automation_message(msg)
+         Job_Log.info("Writing automation message for error #{err.message}")
+         BaseJob.write_automation_message(msg)
       else
-         Job_Log.fatal(message)
+         Job_Log.fatal("Caught fatal error #{err}")
          raise err
       end
    end
 
-   private
-   def write_automation_message(message)
+   def self.write_automation_message(message)
       # If a message makes it to AutomationMessageProcessor without a messagable object
       # (i.e. messagable_id is empty and/or messagable_type is empty) make the default
       # behavior to assign the message to aec6v's StaffMember object.
