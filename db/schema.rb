11953d9159fbe1eec1137d8dc65be2019b9cf78a
lf6f <lf6f@d-172-25-72-99.bootp.Virginia.EDU>
2016-01-27 15:32:11 -0500

    cleanup unused admin_user stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160114210202) do
+ActiveRecord::Schema.define(:version => 20160127165855) do
 
   create_table "academic_statuses", :force => true do |t|
     t.string   "name"
@@ -54,24 +54,6 @@ ActiveRecord::Schema.define(:version => 20160114210202) do
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
   end
-
-  create_table "admin_users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
-  end
-
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "agencies", :force => true do |t|
     t.string   "name"
