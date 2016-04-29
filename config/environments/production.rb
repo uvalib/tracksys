c876f7ab181f55bbed08624358f6749d5fb626ee
lf6f <lf6f@d-172-25-72-99.bootp.Virginia.EDU>
2016-04-29 15:43:43 -0400

    revert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,83 +1,70 @@
 Tracksys::Application.configure do
-   # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in config/application.rb
 
-   # Code is not reloaded between requests.
-   config.cache_classes = true
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
-   # Eager load code on boot. This eager loads most of Rails and
-   # your application in memory, allowing both threaded web servers
-   # and those relying on copy on write to perform better.
-   # Rake tasks automatically ignore this option for performance.
-   config.eager_load = true
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-   # Full error reports are disabled and caching is turned on.
-   config.consider_all_requests_local       = false
-   config.action_controller.perform_caching = true
+  config.eager_load = true
 
-   # Enable Rack::Cache to put a simple HTTP cache in front of your application
-   # Add `rack-cache` to your Gemfile before enabling this.
-   # For large-scale production use, consider using a caching reverse proxy like
-   # NGINX, varnish or squid.
-   # config.action_dispatch.rack_cache = true
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
 
-   # Disable serving static files from the `/public` folder by default since
-   # Apache or NGINX already handles this.
-   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
 
-   # Compress JavaScripts and CSS.
-   config.assets.js_compressor = :uglifier
-   # config.assets.css_compressor = :sass
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
 
-   # Do not fallback to assets pipeline if a precompiled asset is missed.
-   config.assets.compile = false
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
-   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-   # yet still be able to expire them through the digest params.
-   config.assets.digest = true
+  # Defaults to Rails.root.join("public/assets")
+  # config.assets.manifest = YOUR_PATH
 
-   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  # Specifies the header that your server uses for sending files
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
-   # Specifies the header that your server uses for sending files.
-   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
 
-   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-   # config.force_ssl = true
+  # See everything in the log (default is :info)
+  # config.log_level = :debug
 
-   # Use the lowest log level to ensure availability of diagnostic information
-   # when problems arise.
-   config.log_level = :debug
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
 
-   # Prepend all log lines with the following tags.
-   # config.log_tags = [ :subdomain, :uuid ]
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
+  #config.cache_store = :file_store, "#{config.root}/tmp/file_store_cache"
 
-   # Use a different logger for distributed setups.
-   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  # Add additional directories to asset pipeline search paths
+  config.assets.paths << "#{Rails.root}/app/assets/images/request_form"
 
-   # Use a different cache store in production.
-   # config.cache_store = :mem_cache_store
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
 
-   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-   # config.action_controller.asset_host = 'http://assets.example.com'
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w[active_admin.css active_admin.js request.css request.js email.css printable.css]
 
-   # Ignore bad email addresses and do not raise email delivery errors.
-   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-   # config.action_mailer.raise_delivery_errors = false
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
 
-   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-   # the I18n.default_locale when a translation cannot be found).
-   config.i18n.fallbacks = true
+  # Provide path for CSS for Roadie.
+  config.action_mailer.default_url_options = {:host => 'tracksys.lib.virginia.edu'}
 
-   # Send deprecation notices to registered listeners.
-   config.active_support.deprecation = :notify
+  # Enable threaded mode
+  # config.threadsafe!
 
-   # Use default logging formatter so that PID and timestamp are not suppressed.
-   config.log_formatter = ::Logger::Formatter.new
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
 
-   # Do not dump schema after migrations.
-   config.active_record.dump_schema_after_migration = false
-
-   # Provide path for CSS for Roadie.
-   config.action_mailer.default_url_options = {:host => 'tracksys.lib.virginia.edu'}
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
 
 end
