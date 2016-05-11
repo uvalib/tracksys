510b1911ab1859fbab694d6001103cea6f37fb01
lf6f <lf6f@d-172-25-72-99.bootp.Virginia.EDU>
2016-05-11 16:15:12 -0400

    remove misleading comment

--- app/jobs/create_order_email.rb
+++ app/jobs/create_order_email.rb
@@ -2,7 +2,6 @@ class CreateOrderEmail < BaseJob
 
    def do_workflow(message)
 
-      # Validate incoming message.  'internal_dir' is required of all imcoming messages.
       raise "Parameter 'order' is required" if message[:order].blank?
       raise "Parameter 'delivery_files' is required" if message[:delivery_files].blank?
 
