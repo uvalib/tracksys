dba3a6c8bb93cf73e24ead1a7139b7fc92905a0e
Lou Foster <lf6f@virginia.edu>
2016-05-18 14:30:33 -0400

    dj time limit back to 4 hours

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,3 +1,3 @@
-Delayed::Worker.max_run_time = 8.hours
+Delayed::Worker.max_run_time = 4.hours
 Delayed::Worker.destroy_failed_jobs = true
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
