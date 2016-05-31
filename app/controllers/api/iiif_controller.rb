ba1b06078583dc3237073296ee318c56a7c39593
Lou Foster <lf6f@virginia.edu>
2016-05-31 11:45:58 -0400

    cors headers

--- app/controllers/api/iiif_controller.rb
+++ app/controllers/api/iiif_controller.rb
@@ -1,5 +1,14 @@
 class Api::IiifController < ApplicationController
 
+   after_filter :set_cors_headers
+
+   def set_cors_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+   end
+
    # Get the Json IIIF metadata for the bibl
    #
    def show
