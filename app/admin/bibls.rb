2da7e6238d844ae3db6d052f109c36487b592e0f
Lou Foster <lf6f@virginia.edu>
2016-06-21 15:17:11 -0400

    only show download pdf button for items in DL

--- app/admin/bibls.rb
+++ app/admin/bibls.rb
@@ -10,7 +10,7 @@ ActiveAdmin.register Bibl do
 
   config.clear_action_items!
   action_item :pdf, :only => :show do
-     raw("<a href='#{Settings.pdf_url}/#{bibl.pid}' target='_blank'>Download PDF</a>")
+     raw("<a href='#{Settings.pdf_url}/#{bibl.pid}' target='_blank'>Download PDF</a>") if bibl.in_dl?
   end
 
   action_item :new, :only => :index do
