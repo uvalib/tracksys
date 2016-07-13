04ace1b5bacfe6ab1a484905d2c43f9f67cf8e9c
Lou Foster <lf6f@virginia.edu>
2016-07-13 13:42:13 -0400

    cleanup update code

--- app/jobs/propogate_dl_attributes.rb
+++ app/jobs/propogate_dl_attributes.rb
@@ -20,29 +20,26 @@ class PropogateDlAttributes < BaseJob
          on_error "Unit #{unit.id} has no discoverability value.  Please fill in and restart ingestion."
       end
       if unit.indexing_scenario.nil?
-         unit.indexing_scenario = IndexingScenario.find(1)
-         unit.save!
+         unit.update(indexing_scenario: IndexingScenario.find(1) )
          on_failure "Unit #{unit.id} has no indexing scenario selected so it is assumed to use the default scenario."
       end
 
       if object.indexing_scenario.nil?
-         object.indexing_scenario = unit.indexing_scenario
-         object.save!
+         object.update(indexing_scenario: unit.indexing_scenario)
          on_success "Indexing scenario for object #{object.class.name} #{object.id} is changed to #{unit.indexing_scenario.name}."
       else
          on_success "Indexing scenario for object #{object.class.name} #{object.id} is already set to #{object.indexing_scenario.name} and will not be changed."
       end
 
       if object.discoverability.nil?
-         object.discoverability = unit.master_file_discoverability
-         object.save!
+         object.update(discoverability: unit.master_file_discoverability)
          on_success "Discoverability for object #{object.class.name} #{object.id} is changed to #{unit.master_file_discoverability}."
       else
          on_success "Discoverability for object #{object.class.name} #{object.id} is already set to #{object.discoverability} and will not be changed."
       end
 
       # All ingestable objects have a date_dl_ingest attribute which can be updated at this time.
-      object.update_attribute(:date_dl_ingest, Time.now)
+      object.update(date_dl_ingest: Time.now)
 
       # Publish masterfile images to iiif server
       if object.is_a? MasterFile
