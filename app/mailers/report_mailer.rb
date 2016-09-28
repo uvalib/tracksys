cac8ada72d18a6ebd1c634b4bc6e34f1cb031865
Lou Foster <lf6f@virginia.edu>
2016-09-28 14:47:42 -0400

    email noty when stats report done

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -9,4 +9,10 @@ class ReportMailer < ActionMailer::Base
       attachments["dl_manifest_#{Time.now.strftime('%Y%m%d')}.xlsx"] = File.read("#{Rails.root}/tmp/dl_manifest_#{Time.now.strftime('%Y%m%d')}.xlsx")
       mail to: @staff_member.email, subject: "DCS Digital Library Manifest as of #{Time.now.strftime("%B %-d, %Y")}"
    end
+
+   def stats_report_complete(staff_member, path)
+      @staff_member = staff_member
+      @stats_path = path
+      mail to: @staff_member.email, subject: "Stats report ready"
+   end
 end
