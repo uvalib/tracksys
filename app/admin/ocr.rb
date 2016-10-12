17d04ef0504e932f19346127030cf627746fee0f
Lou Foster <lf6f@virginia.edu>
2016-10-12 14:51:54 -0400

    use settings for IIIF url. use it for ocr zoom too

--- app/admin/ocr.rb
+++ app/admin/ocr.rb
@@ -64,15 +64,6 @@ ActiveAdmin.register_page "OCR" do
       render :text=>job_id, :status=>:ok
    end
 
-   page_action :zoom, method: :get do
-      mf = MasterFile.find(params[:mf])
-      jpg_file = Tempfile.new(["#{mf.unit_id}", '.jpg'])
-      arch_file = File.join(ARCHIVE_DIR, "%09d" % mf.unit_id, mf.filename)
-      cmd = "convert -resize 2000 #{arch_file} jpg:-> #{jpg_file.path}"
-      `#{cmd}`
-      send_data IO.binread(jpg_file.path), :type => 'image/jpg',:disposition => 'inline'
-   end
-
    # GET to status to get status of job ID passed as query param
    #
    page_action :status, method: :get do
