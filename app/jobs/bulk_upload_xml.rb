93081498832c52795f5178170967e437e1cb8fb0
Lou Foster <lf6f@virginia.edu>
2016-10-13 16:09:50 -0400

    fix bulk upload; validate XML before using it. update metadata if it already exists

--- app/jobs/bulk_upload_xml.rb
+++ app/jobs/bulk_upload_xml.rb
@@ -22,6 +22,12 @@ class BulkUploadXml < BaseJob
          xml_str = f.read()
          f.close
 
+         errors = XmlMetadata.validate( xml_str )
+         if errors.length > 0
+            on_failure("XML File #{xf} has errors and has been skipped. Errors: #{errors.join(',')}")
+            next
+         end
+
          # Convert the xml file name into a tif file name and find a matching master file
          xml_name = xf.split("/").last
          tif_name = xml_name.split(".")[0] + ".tif"
@@ -31,29 +37,29 @@ class BulkUploadXml < BaseJob
          else
             cnt += 1
 
-            # some desc_metadata has namespaces, some does not.
-            # figure out if this one does, and set params to be used in xpath
-            ns = ""
-            ns = "mods:" if xml_str.include? "xmlns:mods"
-
             # Extract title and creator info
             xml = Nokogiri::XML( xml_str )
+            xml.remove_namespaces!
             title = orig_metadata.title
             creator = orig_metadata.creator_name
-            title_node = xml.xpath( "//#{ns}titleInfo/#{ns}title" ).first
-            if !title_node.nil?
-               title = title_node.text.strip
-            end
-            creator_node = xml.xpath("//#{ns}name/#{ns}namePart").first
-            creator = creator_node.text if !creator_node.nil?
+            title_node = xml.xpath( "//titleInfo/title" ).first
+            title = title_node.text.strip if !title_node.nil?
+            creator_node = xml.xpath("//name/namePart").first
+            creator = creator_node.text.strip if !creator_node.nil?
 
-            # Create the new metadata record and associate it with the masterfile
-            metadata = Metadata.create!(type: "XmlMetadata", title: title, is_approved: orig_metadata.is_approved,
-               discoverability: orig_metadata.discoverability, indexing_scenario_id: 2,
-               desc_metadata: xml_str, use_right_id: orig_metadata.use_right_id,
-               availability_policy: orig_metadata.availability_policy,
-               creator_name: creator, exemplar: mf.filename)
-            mf.update(metadata_id: metadata.id)
+            if mf.metadata == orig_metadata
+               # This Master file is still associated with the original unit metadata.
+               # Create a new metadata record based on the XML and associate it with the masterfile
+               metadata = Metadata.create!(type: "XmlMetadata", title: title, is_approved: orig_metadata.is_approved,
+                  discoverability: orig_metadata.discoverability, indexing_scenario_id: 2,
+                  desc_metadata: xml_str, use_right_id: orig_metadata.use_right_id,
+                  availability_policy: orig_metadata.availability_policy,
+                  creator_name: creator, exemplar: mf.filename)
+               mf.update(metadata_id: metadata.id)
+            else
+               # This masterfile already has its own metadata; just update content
+               mf.metadata.update(desc_metadata: xml_str, title: title, creator_name: creator)
+            end
          end
       end
       logger.info "Updated #{cnt} masterfiles with XML metadata"
