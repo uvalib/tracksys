e6f140dfac9a5cd1bbeb0543ba554cbccaf021bc
Lou Foster <lf6f@virginia.edu>
2016-12-06 12:49:10 -0500

    catch test publish error and log them

--- app/jobs/publish_to_test.rb
+++ app/jobs/publish_to_test.rb
@@ -9,12 +9,20 @@ class PublishToTest < BaseJob
       unit = message[:unit]
       if unit.metadata.discoverability
          logger.info "Publish unit ID #{unit.id} to test"
-         unit.metadata.publish_to_test
+         begin
+            unit.metadata.publish_to_test
+         rescue Exception=>e
+            on_failure("Unable to publish unit #{unit.id} metadata #{unit.metadata.id}: #{e.message}")
+         end
       end
       unit.master_files.each do |mf|
          if mf.metadata.discoverability && unit.metadata.id != mf.metadata.id
             logger.info "Publish master file ID #{mf.id} to test"
-            mf.metadata.publish_to_test
+            begin
+               mf.metadata.publish_to_test
+            rescue Exception=>e
+               on_failure("Unable to publish masterfile #{mf.id} metadata #{mf.metadata.id}: #{e.message}")
+            end
          end
       end
    end
