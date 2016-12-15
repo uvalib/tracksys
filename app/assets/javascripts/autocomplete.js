54ee5be7241d53ca2b54c6ec9a9308e41ed3d48a
Lou Foster <lf6f@virginia.edu>
2016-12-15 10:31:09 -0500

    fixes to unit autocomplete

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
    $('#unit_order_id').autocomplete({
-       serviceUrl: '/admin/orders/autocomplete'
+       serviceUrl: '/admin/orders/autocomplete',
+       deferRequestBy: 250,
+       showNoSuggestionNotice: true
    });
    $('#unit_metadata_id').autocomplete({
-       serviceUrl: '/admin/'+$('#unit_metadata_id').data("queryuri")+'/autocomplete'
+       serviceUrl: '/admin/'+$('#unit_metadata_id').data("queryuri")+'/autocomplete',
+       deferRequestBy: 250,
+       showNoSuggestionNotice: true
    });
 
    // Toggle between Sirsi/Xml metadata
    $("#metadata-type-picker").on("change", function() {
       var metadataType = $(this).val();
-      $('#autocomplete').autocomplete().setOptions( {serviceUrl: '/admin/'+metadataType+'/autocomplete'} );
+      $('#unit_metadata_id').autocomplete().setOptions( {serviceUrl: '/admin/'+metadataType+'/autocomplete'} );
       $("#unit_metadata_id").val("");
    });
 });
