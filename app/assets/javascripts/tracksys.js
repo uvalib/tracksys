a90e8e715516b0cc0ac3d3731942a7f01fd7f17f
Lou Foster <lf6f@virginia.edu>
2017-02-01 09:14:18 -0500

    enter is tab on forms. order id cleared if 0

--- app/assets/javascripts/tracksys.js
+++ app/assets/javascripts/tracksys.js
@@ -1,6 +1,21 @@
 $(function() {
    $("#q_desc_metadata_input select").attr("disabled", "disabled");
 
+
+   $("input").not($(":button")).keypress(function (evt) {
+      if (evt.keyCode == 13) {
+         iname = $(this).val();
+         if (iname !== 'Submit') {
+            var fields = $(this).parents('form:eq(0),body').find('button, input, textarea, select');
+            var index = fields.index(this);
+            if (index > -1 && (index + 1) < fields.length) {
+               fields.eq(index + 1).focus();
+            }
+            return false;
+         }
+      }
+   });
+
    // Colorbox
    $("a[rel='colorbox']").colorbox({width:"100%", maxHeight:"100%"});
 
