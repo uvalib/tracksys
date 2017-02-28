ef66cfc52a719f5bc9c037238837da01d1197711
Lou Foster <lf6f@virginia.edu>
2017-02-28 11:50:08 -0500

    start of some interactivity on skills matrix

--- app/admin/staff_skills.rb
+++ app/admin/staff_skills.rb
@@ -4,4 +4,13 @@ ActiveAdmin.register_page "Staff Skills" do
    content do
       render partial: 'skills_matrix'
    end
+
+   page_action :staff, :method => :get do
+      skill_id = params[:skill]
+      out = []
+      StaffMember.joins(:skills).where("staff_skills.category_id=?", skill_id).each do |s|
+         out << {id: s.id, name: s.full_name }
+      end
+      render json: out
+   end
 end
--- app/assets/javascripts/skills.js
+++ app/assets/javascripts/skills.js
@@ -1,2 +1,50 @@
 $(function() {
+   $("#category").on("change", function() {
+      var id = $(this).val();
+      $("div.staff-skills").empty();
+      $("p.staff").show();
+      $.getJSON("/admin/staff_skills/staff?skill="+id, function ( data, textStatus, jqXHR ){
+         if (textStatus == "success" ) {
+            var template = "<p data-id='ID' class='skilled'>NAME</p>";
+            $.each(data, function(idx,val) {
+               var row = template.replace("ID", val.id).replace("NAME", val.name);
+               $("div.staff-skills").append( $(row) );
+               $("p.staff[data-id='"+val.id+"']").hide();
+            });
+         } else {
+            alert("Unable to retrieve staff with selected skill. Please try again later");
+         }
+      });
+   });
+
+   $(".scroller").on("click",".skilled, .staff", function() {
+      if ( $(this).hasClass("selected") ) {
+         $(this).removeClass("selected");
+      } else {
+         $(this).addClass("selected");
+      }
+   });
+
+   $("#assign-skill").on("click", function() {
+      if ( $("#category").val() === "Select a category" ) {
+         alert("Please select a category.");
+         return;
+      }
+      if ( $("p.staff.selected").length === 0) {
+         alert("Please select staff members to assign to the current category.");
+         return;
+      }
+   });
+
+   $("#unassign-skill").on("click", function() {
+      if ( $("#category").val() === "Select a category" ) {
+         alert("Please select a category.");
+         return;
+      }
+      if ( $("p.skilled.selected").length === 0) {
+         alert("Please select staff members to un-assign from the current category.");
+         return;
+      }
+   });
+
 });
