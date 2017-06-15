32836679b10b1b254e5ab436b682f4a2ed2b6156
Lou Foster <lf6f@virginia.edu>
2017-06-15 13:39:09 -0400

    api to do dpla transform

--- app/controllers/api/metadata_controller.rb
+++ app/controllers/api/metadata_controller.rb
@@ -2,7 +2,7 @@ class Api::MetadataController < ApplicationController
    def show
       render :text=>"type is required", status: :bad_request and return if params[:type].blank?
       type = params[:type].strip.downcase
-      render :text=>"#{type} is not supported", status: :bad_request and return if type != "desc_metadata"
+      render :text=>"#{type} is not supported", status: :bad_request and return if type != "desc_metadata" && type != "dpla_metadata"
       render :text=>"PID is invalid", status: :bad_request and return if !params[:pid].include?(":")
 
       object = Metadata.find_by(pid: params[:pid])
@@ -13,6 +13,20 @@ class Api::MetadataController < ApplicationController
 
       if type == "desc_metadata"
          render :xml=> Hydra.desc(object)
+      end
+
+      if type == "dpla_metadata"
+         xsl = File.join(Rails.root, "lib", "xslt", "DPLA", "MODStoDPLAMODS.xsl")
+         saxon = "java -jar #{File.join(Rails.root, "lib", "Saxon-HE-9.7.0-8.jar")}"
+         params = "pid=#{object.pid} exemplarPid=#{object.pid}"
+
+         tmp = Tempfile.new(object.pid)
+         tmp.write(Hydra.desc(object))
+         tmp.close
+
+         cmd = "#{saxon} -s:#{tmp.path} -xsl:#{xsl} #{params}"   #-o:#{out} #{params}"
+         puts "     #{cmd}"
+         render :xml=> `#{cmd}`
       end
    end
 end
