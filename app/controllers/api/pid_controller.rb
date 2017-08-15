7349a34b4ec915a0ca9793b781a86fc283773925
Lou Foster <lf6f@virginia.edu>
2017-08-15 11:12:00 -0400

    more text fixes

--- app/controllers/api/pid_controller.rb
+++ app/controllers/api/pid_controller.rb
@@ -1,6 +1,6 @@
 class Api::PidController < ApplicationController
    def show
-      render :text=>"PID is invalid", status: :bad_request and return if !params[:pid].include?(":")
+      render :plain=>"PID is invalid", status: :bad_request and return if !params[:pid].include?(":")
 
       # check each model that can have a PID to find a match. start
       # with most likely (metadata) and proceed down (masterfile, component)
@@ -18,7 +18,7 @@ class Api::PidController < ApplicationController
          object = Component.find_by(pid: params[:pid])
       end
       if object.nil?
-         render :text=>"Could not find PID", status: :not_found
+         render :plain=>"Could not find PID", status: :not_found
       else
          render :json=>{id: object.id, type: type, url: "#{Settings.tracksys_url}/admin/#{url_frag}/#{object.id}"}, status: :ok
       end
