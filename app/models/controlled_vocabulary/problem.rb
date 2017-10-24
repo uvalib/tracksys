558e163f3290067357f952bf1ea827c5ae509e9d
Lou Foster <lf6f@virginia.edu>
2017-10-24 09:35:59 -0400

    remove automation problems from checkbox list. place Other last

--- app/models/controlled_vocabulary/problem.rb
+++ app/models/controlled_vocabulary/problem.rb
@@ -9,4 +9,9 @@
 
 class Problem < ApplicationRecord
    validates :name, :uniqueness => true, :presence => true
+   def self.qa
+      out = Problem.where("not(id >= 5 and id <= 7) and id < 11").order(name: :asc).to_a
+      out << Problem.find_by(name: "Other")
+      return out
+   end
 end
