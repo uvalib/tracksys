d476fbbb58a23f2b35b8c9ed41b489a8868c9dc9
Lou Foster <lf6f@virginia.edu>
2018-02-13 14:31:37 -0500

    more parts for adding tags

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,6 +1,18 @@
 $(function() {
    $("#tag-finder").chosen({
        no_results_text: "Sorry, no matches found",
-       width: "200px"
+       width: "250px"
+   });
+
+   $("#add-tag").on("click", function() {
+      $(".add-tags").show();
+      $("#add-done").show();
+      $("#add-tag").hide();
+   });
+
+   $("#add-done").on("click", function() {
+      $(".add-tags").hide();
+      $("#add-done").hide();
+      $("#add-tag").show();
    });
 });
