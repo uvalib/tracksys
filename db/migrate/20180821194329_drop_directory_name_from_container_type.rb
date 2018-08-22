d38e4a8293499e1a21a4b61fb5104d98a8f57f73
Lou Foster <lf6f@virginia.edu>
2018-08-22 09:34:48 -0400

    fix container names

--- db/migrate/20180821194329_drop_directory_name_from_container_type.rb
+++ db/migrate/20180821194329_drop_directory_name_from_container_type.rb
@@ -1,5 +1,25 @@
 class DropDirectoryNameFromContainerType < ActiveRecord::Migration[5.2]
-  def change
+   def up
       remove_column :container_types, :directory_name, :string
-  end
+      c = ContainerType.find_by(name: "Flat File Drawer")
+      if !c.nil?
+         c.update(name: "Oversize Box")
+      end
+      c = ContainerType.find_by(name: "Tray")
+      if !c.nil?
+         c.update(name: "Flat File Drawer")
+      end
+      c = ContainerType.find_by(name: "Ledger")
+      if !c.nil?
+         c.update(name: "Bound Volume")
+      end
+      c = ContainerType.find_by(name: "Temporary Container")
+      if !c.nil?
+         c.update(name: "Temporary Location")
+      end
+   end
+
+   def down
+      add_column :container_types, :directory_name, :string
+   end
 end
