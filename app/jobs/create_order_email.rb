f854dc99830547b28d07bbd65b1b3c60460ef195
Lou Foster <lf6f@virginia.edu>
2019-02-05 15:55:53 -0500

    remove debugging. fix a few missed logging renames

--- app/jobs/create_order_email.rb
+++ app/jobs/create_order_email.rb
@@ -1,7 +1,6 @@
 class CreateOrderEmail < BaseJob
 
    def set_originator(message)
-      byebug
       @status.update_attributes( :originator_type=>"Order", :originator_id=>message[:order].id )
    end
 
