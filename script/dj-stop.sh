463af5e022e8a36f022c2f2bb3bfbec9db7ca895
Lou Foster <lf6f@virginia.edu>
2019-03-05 09:44:49 -0500

    conditionally source rvm from multiple locations (if available) for dj scripts

--- script/dj-stop.sh
+++ script/dj-stop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-source "/usr/local/rvm/scripts/rvm"
+
+# If present, source RVM so it is availble to this script
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm"
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  source "/usr/local/rvm/scripts/rvm"
+fi
 RAILS_ENV=production script/delayed_job -n8 stop
