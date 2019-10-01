3c18c8288cb93f83bfffff74bf7c2732c5f096b5
Lou Foster <lf6f@virginia.edu>
2019-10-01 13:24:39 -0400

    added delayed job cfg that may help with locking problems/timeouts

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -4,3 +4,6 @@ Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.lo
 
 # QUIET LOGGING FOR QUERIES IN DEV MODE
 ActiveRecord::Base.logger.level = 1 # or Logger::INFO
+
+# from a not on gem site. Users having issues locking jobs should try this. Could help timeout
+Delayed::Backend::ActiveRecord.configuration.reserve_sql_strategy = :default_sql
